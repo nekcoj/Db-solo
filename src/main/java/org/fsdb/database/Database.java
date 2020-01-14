3afe9f77cbbe91ca4f934b2c4937b697f44f2f47
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-01-14 11:43:35 +0100

    created new branch to hopefully solve issues with database-createFilesAndFoldersFromJSON

--- src/main/java/org/fsdb/database/Database.java
+++ src/main/java/org/fsdb/database/Database.java
@@ -1,70 +1,61 @@
 package org.fsdb.database;
 
-import org.fsdb.FileSystem;
+import com.github.cliftonlabs.json_simple.*;
 import org.fsdb.Util;
 import org.fsdb.database.query.Query;
 import org.fsdb.database.query.QueryResult;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ArrayList;
+import static org.fsdb.FileSystem.createDir;
+import static org.fsdb.FileSystem.writeFile;
+
 import java.io.File;
 import java.util.HashMap;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class Database {
     private String dbName;
+    public Database() {
+        List<String> dbFiles = new ArrayList<>();
+        Path currentDir = Paths.get(".");
+        String absPath = currentDir.toAbsolutePath().toString();
+        String path[] = absPath.split("\\.");
+        List<String> pathList;
+        pathList = Arrays.asList(path);
+        pathList.get(0);
+        dbFiles.add("" + pathList.get(0) + "/songs.json");
+        dbFiles.add("" + pathList.get(0) +"/albums.json");
+        dbFiles.add("" + pathList.get(0) +"/artists.json");
+        String dbDir = "" + pathList.get(0) + "/MusicLib";
+        createDatabaseDir(dbFiles, dbDir);
+    }
 
     public boolean create(String name) {
-        dbName = name;
-        return FileSystem.createDir(name);
+        return createDir(name);
     }
 
     public QueryResult executeQuery(Query query) {
         QueryResult result;
-        String rootDir = dbName + "/" + query.rootName;
 
         switch (query.action) {
-            case FETCH: {
-                var found = findWith(rootDir, query.predicateField, query.predicateValue);
-                if (found == null) {
-                    result = null;
-                    break;
-                }
-                result = new QueryResult(true, query.action, found.second);
-                break;
-            }
-            case DELETE: {
-                var found = findWith(rootDir, query.predicateField, query.predicateValue);
-                if (found == null) {
-                    result = null;
-                    break;
-                }
-                boolean wasDeleted = FileSystem.delete(found.first);
-                result = new QueryResult(wasDeleted, query.action);
-                break;
-            }
+            case FETCH:
+            case DELETE:
             case CREATE: {
-                String fileName = rootDir + "/" + query.values.get("id");
-                String data = serializeData(query.values);
-                if (!FileSystem.exists(fileName)) {
-                    FileSystem.createDir(rootDir);
-                }
-                FileSystem.writeFile(fileName, data);
-                result = new QueryResult(true, query.action, query.values);
+                result = new QueryResult(true, query.action);
                 break;
             }
             case UPDATE: {
-                var found = findWith(rootDir, query.predicateField, query.predicateValue);
-                if (found == null) { // handle properly
-                    result = null;
-                    break;
-                }
-                // loop over and update given values
-                for (Map.Entry<String, String> entry : query.values.entrySet()) {
-                    if (found.second.containsKey(entry.getKey())) {
-                        found.second.put(entry.getKey(), entry.getValue());
-                    }
-                }
-                FileSystem.writeFile(found.first, serializeData(found.second));
                 result = new QueryResult(true, query.action, query.values);
                 break;
             }
@@ -77,7 +68,7 @@ public class Database {
         return result;
     }
 
-    public HashMap<String, String> deserializeData(String fileData) {
+    public HashMap<String, String> parseData(String fileData) {
         List<String> lines = Util.stringToLines(fileData);
         if (lines == null) return null;
 
@@ -91,30 +82,39 @@ public class Database {
         return values;
     }
 
-    public String serializeData(HashMap<String, String> data) {
-        StringBuilder str = new StringBuilder();
-        for (Map.Entry<String, String> entry : data.entrySet()) {
-            str.append(String.format("%s=%s\n", entry.getKey(), entry.getValue()));
+    public void createDatabaseDir(List<String> pathNames, String dbDir){
+        createDir(dbDir);
+        for (String s : pathNames) {
+            String a[] = s.split("\\.");
+
+            List<String> pathList;
+            pathList = Arrays.asList(a);
+
+            String[] dbPathSplit = pathList.get(0).split("/");
+
+            List<String> getFilePath = Arrays.asList(dbPathSplit);
+            String filePath = dbDir + "/" + getFilePath.get(getFilePath.size()-1);
+
+            createDir(filePath);
+            createFileFromJSON(s,filePath);
         }
-        return str.toString().strip();
     }
+    public void createFileFromJSON(String filePath, String dirPath){
+        try(FileReader fileReader = new FileReader(filePath)){
+            Object deserialize = Jsoner.deserialize(fileReader);
+            JsonArray ja = (JsonArray) deserialize;
 
-    private Tuple<String, HashMap<String, String>> findWith(String root, String field, String value) {
-        File[] dirFiles = FileSystem.getDirFiles(root);
-        if (dirFiles == null) return null;
-
-        String fileName = null;
-        HashMap<String, String> result = null;
-
-        for (File file : dirFiles) {
-            String content = FileSystem.readFile(file.getPath());
-            fileName = file.getPath();
-            var data = deserializeData(content);
-            if (data.get(field).equals(value)) {
-                result = data;
-                break;
+            for (Object ob: ja) {
+                JsonObject temp = (JsonObject) ob;
+                String fileName = temp.get("_id").toString();
+                writeFile(dirPath+"/"+fileName, temp.toString());
             }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (JsonException e) {
+            e.printStackTrace();
         }
-        return new Tuple<>(fileName, result);
+
     }
 }
